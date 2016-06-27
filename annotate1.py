22a3e9f3eda3b6cf835ac5f8ca65a97e967472e5
Aida-Rahmattalabi <rahmatta@onid.oregonstate.edu>
2016-06-27 16:00:05 -0700

    Update annotate1.py

--- annotate1.py
+++ annotate1.py
@@ -20,6 +20,7 @@ from tf.transformations import euler_from_quaternion
 import math 
 import numpy as np
 import time
+
 class turn:
     def __init__(self, threshDis):
         # Set the stopping distance and max speed
@@ -50,7 +51,7 @@ class turn:
 
     def check_alignement(self, pose_data):
 
-	#-----------------------------Find Robots Orientation--------------------------
+	#-----------------------------Find Robot's Orientation--------------------------
     	quaternion = (pose_data.pose.pose.orientation.x,
 			pose_data.pose.pose.orientation.y, 
 			pose_data.pose.pose.orientation.z, 
@@ -95,15 +96,18 @@ class turn:
 		if yaw < 0:
 			yaw = yaw + 2*math.pi
 		#-------------------------------------------------------------------------------	
-		if abs(yaw - round(yaw/(math.pi/2))*(math.pi/2))>=0.06:
-			command.angular.z = -0.1 *np.sign(yaw - round(yaw/(math.pi/2))*(math.pi/2))
-			pub.publish(command) 
+		if abs(yaw - round(yaw/(math.pi/2))*(math.pi/2))>=0.07:
+			command.angular.z = -0.1 *np.sign(yaw - round(yaw/(math.pi/2))*(math.pi/2))  
+			self.pub.publish(command) 
 
-		if abs(yaw - round(yaw/(math.pi/2))*(math.pi/2))<0.06:
+		if abs(yaw - round(yaw/(math.pi/2))*(math.pi/2))<0.07:
 			self.aligned = True
 #-----------------------------------------------------------------------------
     def callBack(self,pose_data,sensor_data):
-	
+	#refined_sensor_data = sensor_data.ranges[71:569] #
+	#maxim = np.nanmax(refined_sensor_data)#
+	#print maxim
+
 	#self.distance=rospy.get_param('turn')
 	if self.check_aligned:
 		self.check_alignement(pose_data)
@@ -139,12 +143,12 @@ class turn:
 		command.linear.z = 0.0
 		command.angular.x = 0.0
 		command.angular.y = 0.0
-		command.angular.z = 0.3
+		command.angular.z = 0.5
 
 		delta =  self.orientation2 - self.orientation1
 		if delta > 2*math.pi:
 			delta = delta - 2*math.pi
-		print self.orientation1, self.orientation2, delta
+		#print self.orientation1, self.orientation2, delta
 		if abs(delta) >= math.pi/2.00:
 			stop = True
 		if stop:
@@ -154,17 +158,17 @@ class turn:
 			stop = False
 			self.orientation1 = -1000
 
-			maxIndice = np.argmax(refined_sensor_data)
-			print maxIndice, refined_sensor_data[maxIndice]
+			maxim = np.nanmax(refined_sensor_data)
+			print maxim
 
-			if refined_sensor_data[maxIndice] > self.distance :
+			if maxim > self.distance :
 				self.vect.append(1)
 				self.check_aligned = True
 			else:
 				self.vect.append(0)
 				self.check_aligned = True
 		#time.sleep(5)
-		pub.publish(command) 
+		self.pub.publish(command) 
 		i1 = refined_sensor_data[0]
 
 		for i in refined_sensor_data:
@@ -183,11 +187,8 @@ class turn:
 if __name__ == '__main__':
     	
 	rospy.init_node('annotate_map')
-	input_var=3
+	input_var=8.5
 	robot =turn(input_var)
-	# A publisher for the move data
-	pub = rospy.Publisher('/mobile_base/commands/velocity',
-	          Twist,
-	          queue_size=10)
+
 	
 	rospy.spin()
